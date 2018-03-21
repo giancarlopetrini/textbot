c4fafb85c344c7baea517b7a6bbd03521153ad9b
Giancarlo Petrini <giancarlo@libreworx.com>
2018-03-21 14:43:59 -0400

    migrate tfstate to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,68 @@ provider "aws" {
   region  = "us-east-1"
   profile = "tfbuild"
 }
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "terraform-state-s3-giancarlopetrini"
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  tags {
+    Name = "terraform-state-s3-giancarlopetrini"
+  }
+}
+
+resource "aws_s3_bucket_policy" "terraform-state-policy" {
+  bucket = "${aws_s3_bucket.terraform-state.id}"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [{
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::511386816865:user/tfbuild"
+            },
+            "Action": "s3:ListBucket",
+            "Resource": "arn:aws:s3:::terraform-state-s3-giancarlopetrini"
+        },
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::511386816865:user/tfbuild"
+            },
+            "Action": ["s3:GetObject", "s3:PutObject"],
+            "Resource": "arn:aws:s3:::terraform-state-s3-giancarlopetrini/terraform.tfstate"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state" {
+  name           = "dynamodb-terraform-state-lock"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags {
+    Name = "dynamodb-terraform-state-lock"
+  }
+}
+
+resource "aws_s3_bucket" "lambda-file" {
+  bucket        = "giancarlopetrini-lambda-file"
+  force_destroy = true
+
+  tags {
+    Name = "giancarlopetrini-lambda-file"
+  }
+}
---
+++ terraform/terraform.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "terraform-state-s3-giancarlopetrini"
+    dynamodb_table = "dynamodb-terraform-state-lock"
+    region         = "us-east-1"
+    key            = "terraform.tfstate"
+    profile        = "tfbuild"
+  }
+}
