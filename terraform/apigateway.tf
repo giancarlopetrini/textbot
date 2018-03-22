88c1f2778d54375887311a5756cbb3866d6f92ce
Giancarlo Petrini <giancarlo@libreworx.com>
2018-03-22 17:28:27 -0400

    adjust iam assume role on lambda

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -9,17 +9,17 @@ resource "aws_api_gateway_resource" "lex-resource" {
   path_part   = "dev"
 }
 
-resource "aws_api_gateway_method" "lex-post" {
+resource "aws_api_gateway_method" "lex-method" {
   rest_api_id   = "${aws_api_gateway_rest_api.lex-gateway-api.id}"
   resource_id   = "${aws_api_gateway_resource.lex-resource.id}"
-  http_method   = "POST"
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lex-integration" {
   rest_api_id = "${aws_api_gateway_rest_api.lex-gateway-api.id}"
   resource_id = "${aws_api_gateway_resource.lex-resource.id}"
-  http_method = "${aws_api_gateway_method.lex-post.http_method}"
+  http_method = "${aws_api_gateway_method.lex-method.http_method}"
   type        = "AWS_PROXY"
   uri         = "${aws_lambda_function.lex-lambda.invoke_arn}"
 
@@ -27,6 +27,10 @@ resource "aws_api_gateway_integration" "lex-integration" {
 }
 
 resource "aws_api_gateway_deployment" "lex-gateway" {
+  depends_on = [
+    "aws_api_gateway_integration.lex-integration",
+  ]
+
   rest_api_id = "${aws_api_gateway_rest_api.lex-gateway-api.id}"
   stage_name  = "dev"
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,29 +3,17 @@ resource "aws_iam_role" "lex-lambda-role" {
 
   assume_role_policy = <<POLICY
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": [
-                "*"
-            ]
-        },
-	    {
-            "Effect": "Allow",
-            "Action": [
-                "lex:PostText"
-            ],
-            "Resource": [
-                "*"
-            ]
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
 POLICY
 }
