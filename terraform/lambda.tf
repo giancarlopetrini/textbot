f8093547271a74b87cbde2d1a0691c8fb1d090c7
Giancarlo Petrini <giancarlo@libreworx.com>
2018-03-21 20:09:47 -0400

    add lambda resource

---
+++ terraform/lambda.tf
@@ -0,0 +1,40 @@
+resource "aws_iam_role" "lex-lambda-role" {
+  name = "lex-lambda-role"
+
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+	    {
+            "Effect": "Allow",
+            "Action": [
+                "lex:PostText"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_lambda_function" "lex-lambda" {
+  function_name = "lex-handler"
+  s3_bucket     = "${aws_s3_bucket.lambda-file.bucket}"
+  s3_key        = "lambda.zip"
+  handler       = "lambda"
+  runtime       = "go1.x"
+  role          = "${aws_iam_role.lex-lambda-role.arn}"
+}
