c262ecfb216d8a0a10445e342f0d45f40f0d367f
giancarlopetrini <gpetrini5@gmail.com>
2018-03-24 19:40:39 -0400

    trim strings to pull number and body text from twilio

--- lambda/lambda.go
+++ lambda/lambda.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lexruntimeservice"
 	"github.com/sfreiberg/gotwilio"
 )
 
@@ -26,37 +31,41 @@ type Response struct {
 
 // Handler - invoked by twilio request to api gateway
 func Handler(request Request) (Response, error) {
-	fmt.Println(request.Body)
-	return Response{Res: "Nailed It"}, nil
+	if request.Body == "" {
+		log.Fatalf("No text received from twilio")
+		return Response{}, nil
+	}
+	// maybe add .WithCredentials if needed
+	svc := lexruntimeservice.New(session.New(), aws.NewConfig().WithRegion("us-east-1"))
+	botAlias := "dev"
+	botName := "BookTrip"
+	inputText := strings.TrimRight(request.Body, "&Body")
+	sessionAttr := make(map[string]*string)
+	userID := strings.TrimLeft(strings.TrimRight(request.Body, "&"), "=")
+	input := lexruntimeservice.PostTextInput{
+		BotAlias:          &botAlias,
+		BotName:           &botName,
+		InputText:         &inputText,
+		SessionAttributes: sessionAttr,
+		UserId:            &userID,
+	}
+	outputText, err := svc.PostText(&input)
+	if err != nil {
+		log.Printf("Error getting PostTextOuput from Lex: %s", err)
+	}
+	_, exception, err := twilio.SendSMS(os.Getenv("twilio_num"), userID, *outputText.Message, "", "")
+	if exception != nil {
+		log.Printf("Exception thrown by Twilio while sending response from Lex: %v", exception)
+		return Response{Res: fmt.Sprint(exception)}, nil
+	}
+	if err != nil {
+		log.Printf("Error calling twilio.SendSMS method: %s", err)
+		return Response{Res: fmt.Sprint(err)}, err
+	}
+	return Response{}, nil
+
 }
 
 func main() {
 	lambda.Start(Handler)
 }
-
-// maybe add .WithCredentials if needed
-/*svc := lexruntimeservice.New(session.New(), aws.NewConfig().WithRegion("us-east-1"))
-botAlias := "dev"
-botName := "BookTrip"
-inputText := smsResponse.Body
-sessionAttr := make(map[string]*string)
-userID := strings.Replace(smsResponse.From, "+", "", -1)
-input := lexruntimeservice.PostTextInput{
-	BotAlias:          &botAlias,
-	BotName:           &botName,
-	InputText:         &inputText,
-	SessionAttributes: sessionAttr,
-	UserId:            &userID,
-}
-outputText, err := svc.PostText(&input)
-if err != nil {
-	log.Printf("Error getting PostTextOuput from Lex: %s", err)
-}
-_, except, err = twilio.SendSMS(os.Getenv("twilio_num"), smsResponse.From, *outputText.Message, "", "")
-if except != nil {
-	log.Printf("Exception thrown by Twilio while sending response from Lex: %v", except)
-}
-if err != nil {
-	log.Printf("Error calling twilio.SendSMS method: %s", err)
-}
-return nil*/
