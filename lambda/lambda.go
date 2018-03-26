1cf213614141b185251ceca2a2965d4021518724
giancarlopetrini <gpetrini5@gmail.com>
2018-03-26 17:35:42 -0400

    print accountsid

--- lambda/lambda.go
+++ lambda/lambda.go
@@ -38,7 +38,7 @@ func Handler(request Request) error {
 	if err != nil {
 		return errors.New("Unable to parse input from twilio: , " + fmt.Sprint(err))
 	}
-	log.Println(fmt.Sprint(twilioIn["AccountSid"]))
+	log.Println(twilioIn["AccountSid"])
 	if fmt.Sprint(twilioIn["AccountSid"]) != twilioSid {
 		return errors.New("Sending Twilio <" + fmt.Sprint(twilioIn["AccountSID"]) + ">account not authorized for Lex usage")
 	}
